c6fc3ad255761ff523010b3bd392d553f89ba2c3
Nariman Abdullin <nulls.narik@gmail.com>
2022-08-30 13:17:46 +0300

    Example of save.toml for pylint #3

--- another_test.py
+++ another_test.py
@@ -1,10 +1,12 @@
+# CHECK-MESSAGES:[[1]]: C0114: Missing module docstring (missing-module-docstring)
 import tkinter as tk
 
-root= tk.Tk()
+root=tk.Tk()
 
 canvas1 = tk.Canvas(root, width = 300, height = 300)
 canvas1.pack()
 
+# CHECK-MESSAGES:[[10]]: C0116: Missing function or method docstring (missing-function-docstring)
 def hello ():
     label1 = tk.Label(root, text= 'Hello World!', fg='blue', font=('helvetica', 12, 'bold'))
     canvas1.create_window(150, 200, window=label1)
--- simple_test.py
+++ simple_test.py
@@ -1,1 +1,2 @@
+# CHECK-MESSAGES:[[1]]: C0114: Missing module docstring (missing-module-docstring)
 print("Hello, World!")
